347b90e637f024af779934cb7acdfaac5be67805
PREETI <preetikadyan634@gmail.com>
2021-04-05 16:25:52 +0530

    Update typewise_alert.test.py

--- typewise_alert.test.py
+++ typewise_alert.test.py
@@ -16,6 +16,7 @@ class TypewiseTest(unittest.TestCase):
     
   def test_check_and_alert(self):
     self.assertTrue(typewise_alert.check_and_alert('TO_CONTROLLER',{'coolingType':'PASSIVE_COOLING'},70)== True)
+    self.assertTrue(typewise_alert.check_and_alert('TO_CONSOLE',{'coolingType':'HI_ACTIVE_COOLING'},-10)== True)
     
 
 if __name__ == '__main__':
