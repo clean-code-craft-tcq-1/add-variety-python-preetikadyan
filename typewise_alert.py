1ff55926c3465c2b8366920e25cecc1f2d5ea74f
PREETI <preetikadyan634@gmail.com>
2021-04-08 18:34:09 +0530

    Update typewise_alert.py

--- typewise_alert.py
+++ typewise_alert.py
@@ -16,8 +16,9 @@ def infer_breach(value, lowerLimit, upperLimit):
   return 'NORMAL'
 
 def classify_temperature_breach(coolingType, temperatureInC):
-  coolingType_range = CoolingTypeLimit[coolingType]
-  return infer_breach(temperatureInC, coolingType_range['lowerLimit'], coolingType_range['upperLimit'])
+  if coolingType in CoolingTypeLimit.keys():
+     coolingType_range = CoolingTypeLimit[coolingType]
+     return infer_breach(temperatureInC, coolingType_range['lowerLimit'], coolingType_range['upperLimit'])
 
 
 def check_and_alert(alertTarget, batteryChar, temperatureInC):
