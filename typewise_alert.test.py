021e1605e96e359ed3e270c854c3beece9ecea02
PREETI <preetikadyan634@gmail.com>
2021-04-18 14:34:48 +0530

    Update typewise_alert.test.py

--- typewise_alert.test.py
+++ typewise_alert.test.py
@@ -15,15 +15,15 @@ class TypewiseTest(unittest.TestCase):
     self.assertTrue(typewise_alert.classify_temperature_breach('MED_ACTIVE_COOLING',20) == 'NORMAL')
     self.assertTrue(typewise_alert.classify_temperature_breach('',20) == False)
     
-  def test_check_and_alert(self):
-    self.assertTrue(typewise_alert.check_and_alert('TO_CONTROLLER',{'coolingType':'PASSIVE_COOLING'},70)== 'CONTROLLER_CALLED')
-    #self.assertTrue(typewise_alert.check_and_alert('TO_CONSOLE',{'coolingType':'HI_ACTIVE_COOLING'},-10)== True)
-    self.assertTrue(typewise_alert.check_and_alert('TO_EMAIL',{'coolingType':'MED_ACTIVE_COOLING'},50)== 'EMAIL_SENT')
-    
   def test_empty_or_wrong_alert_target(self):
     self.assertTrue(typewise_alert.check_and_alert('TO_MAIL',{'coolingType':'MED_ACTIVE_COOLING'},50)== False)
     self.assertTrue(typewise_alert.check_and_alert('',{'coolingType':'MED_ACTIVE_COOLING'},50)== False)
     
+  def test_check_and_alert(self):
+    self.assertTrue(typewise_alert.check_and_alert('TO_CONTROLLER',{'coolingType':'PASSIVE_COOLING'},70)== 'CONTROLLER_CALLED')
+    #self.assertTrue(typewise_alert.check_and_alert('TO_CONSOLE',{'coolingType':'HI_ACTIVE_COOLING'},-10)== True)
+    self.assertTrue(typewise_alert.check_and_alert('TO_EMAIL',{'coolingType':'MED_ACTIVE_COOLING'},50)== 'EMAIL_SENT')
+      
 
 if __name__ == '__main__':
   unittest.main()
