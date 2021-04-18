eb1c9a2b464ab1da2aada8f83f5ab297ce36b6bb
PREETI <preetikadyan634@gmail.com>
2021-04-18 14:31:08 +0530

    Update typewise_alert.py

--- typewise_alert.py
+++ typewise_alert.py
@@ -8,6 +8,30 @@ EMailInfo = { 'TOO_LOW' : {'Recepient':'a.b@c.com','Message':'Hi, the temperatur
               'TOO_HIGH' : {'Recepient':'a.b@c.com','Message':'Hi, the temperature is too high'}
             }
 
+ControllerInfo = { 'TOO_LOW': 'Heater On',
+                  'TOO_HIGH': 'Cooler On'
+        }
+
+def compose_email(breachType):
+    return { 'To': EMailInfo[breachType]['Recepient'],
+             'Subject': breachType,
+             'Message':EMailInfo[breachType]['Message']  
+            }
+    
+def compose_controller(breachType):
+    return ControllerInfo[breachType]
+                          
+def email_handle(emailContent):
+   print(emailContent)
+   return "EMAIL_SENT"    
+    
+def send_email(emailContent):
+   return email_handle(emailContent)
+
+def send_controller(controllerContent,breachType):
+    print(f' BreachType: {breachType}action: {controllerContent}')
+    return 'CONTROLLER_CALLED'
+
 def infer_breach(value, lowerLimit, upperLimit):
   if value < lowerLimit:
     return 'TOO_LOW'
@@ -27,17 +51,16 @@ def check_and_alert(alertTarget, batteryChar, temperatureInC):
   breachType = classify_temperature_breach(batteryChar['coolingType'], temperatureInC)
   if alertTarget in Alert_Target.keys():
     Alert_Target[alertTarget](breachType)
-    return True
   return False
 
 def send_to_controller(breachType):
-  print(f'BreachType: {breachType}')
+  return send_controller(compose_controller(breachType),breachType)
 
 def send_to_email(breachType):
-  print(f'{EMailInfo[breachType]}')
+  return send_email(compose_email(breachType))
 
 def send_to_console(breachType):
-  print(f'BreachType: {breachType}')
+  return f' Console Called --> BreachType: {breachType}'
   
 Alert_Target = { 'TO_CONTROLLER': send_to_controller,
                 'TO_EMAIL': send_to_email,
